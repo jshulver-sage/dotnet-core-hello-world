4dc1bad998fdf556ee97d6e29285a970e5891dd7
Jaron Shulver <Jaron.Shulver@sage.com>
2018-02-02 16:30:38 +0000

    Added a dependency

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace HelloWeb
 {
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,25 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace HelloWeb
 {
+    public class MyClass {
+      public string MyString;
+    }
+
     public class Startup
     {
         public void Configure(IApplicationBuilder app)
         {
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Hello World!");
+                MyClass output = new MyClass();
+                output.MyString = "Hello there!";
+
+                string json = JsonConvert.SerializeObject(output);
+                return context.Response.WriteAsync(json);
             });
         }
     }
